4ef6f8281d3a347beb305d2be17ffe4825467b79
Gauresh Tambe <gaureshtambe25@gmail.com>
2026-02-22 23:22:33 +0530

    fix: add basePath in nextra config

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -6,6 +6,7 @@ const withNextra = nextra({})
 // Export the final Next.js config with Nextra included
 export default withNextra({
   assetPrefix: 'https://docsy-docs.vercel.app',
+  basePath: '/docs',
   turbopack: {
     resolveAlias: {
       // Path to your `mdx-components` file with extension
