182831d88acf92ad8187e5867540ccc0bcd036ef
Gauresh Tambe <gaureshtambe25@gmail.com>
2026-02-22 22:40:58 +0530

    fix: removing nextra@2 version code

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,11 +1,7 @@
 import nextra from 'nextra'
 
 // Set up Nextra with its configuration
-const withNextra = nextra({
-  // ... Add Nextra-specific options here
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = nextra({})
 
 // Export the final Next.js config with Nextra included
 export default withNextra({
