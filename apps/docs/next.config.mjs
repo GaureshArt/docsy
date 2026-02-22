28404c4afa5b2ea198bb74fc72b7b1c14377ec6d
Gauresh Tambe <gaureshtambe25@gmail.com>
2026-02-22 22:37:29 +0530

    fix(docs): fix css stylying problem on deployement

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -3,10 +3,14 @@ import nextra from 'nextra'
 // Set up Nextra with its configuration
 const withNextra = nextra({
   // ... Add Nextra-specific options here
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
 })
 
 // Export the final Next.js config with Nextra included
 export default withNextra({
+  basePath: '/docs',
+  assetPrefix: '/docs',
   turbopack: {
     resolveAlias: {
       // Path to your `mdx-components` file with extension
